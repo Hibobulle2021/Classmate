225b2e15f7e22137e9006c8e8eaec2078392d029
GrillednRoasted <107199971+GrillednRoasted@users.noreply.github.com>
2022-06-10 08:14:16 -0700

    changed format
    made it as a function

--- indoor_Hibo.py
+++ indoor_Hibo.py
@@ -1,6 +1,7 @@
-
-text = input("Text: ")
-
-lower_test = text.lower()
-
-print(lower_test)
+def indoor():
+  text = input("Text: ")
+  lower_test = text.lower()
+  print(lower_test)
+  
+if __name__ = '__main__':
+  indoor()
